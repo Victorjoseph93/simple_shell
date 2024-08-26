661276cc17330209f97d7f7c64f268dc0fa5ba76
Chris Ameh <141955256+Pharmchrisameh1@users.noreply.github.com>
2024-08-26 07:01:41 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- **_strncpy - function copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ **_strncpy - function copies a string.
+ *@dest: the destination string to be copied to.
+ *@src: the source string.
+ *@n: the amount of characters to be copied.
+ *Return: the concatenated string.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -27,11 +27,11 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- **_strncat - function that concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: returns the concatenated string
+ **_strncat - function that concatenates two strings.
+ *@dest: the first string.
+ *@src: the second string.
+ *@n: the amount of bytes to be maximally used.
+ *Return: returns the concatenated string.
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -52,10 +52,10 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - function to locate a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ **_strchr - function to locate a character in a string.
+ *@s: the string to be parsed.
+ *@c: the character to look for.
+ *Return: (s) a pointer to the memory area s.
  */
 char *_strchr(char *s, char c)
 {
