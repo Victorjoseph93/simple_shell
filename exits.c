7d59db164a45a34da780e6031c84831567c9f255
Chris Ameh <141955256+Pharmchrisameh1@users.noreply.github.com>
2024-09-06 11:56:34 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- **_strncpy - function copies a string.
- *@dest: the destination string to be copied to.
- *@src: the source string.
- *@n: the amount of characters to be copied.
- *Return: the concatenated string.
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -18,7 +18,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
-
 	if (i < n)
 	{
 		j = i;
@@ -27,17 +26,16 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[j] = '\0';
 			j++;
 		}
-
 	}
 	return (s);
 }
 
 /**
- **_strncat - function that concatenates two strings.
- *@dest: the first string.
- *@src: the second string.
- *@n: the amount of bytes to be maximally used.
- *Return: returns the concatenated string.
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -60,10 +58,10 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - function to locate a character in a string.
- *@s: the string to be parsed.
- *@c: the character to look for.
- *Return: (s) a pointer to the memory area s.
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
@@ -73,4 +71,3 @@ char *_strchr(char *s, char c)
 	} while (*s++ != '\0');
 
 	return (NULL);
-}
